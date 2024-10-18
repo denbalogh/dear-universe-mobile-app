db866b70bb2d2ec661b6e2c24dc42ab1ece748eb
Denis Balogh <denbalogh@gmail.com>
2024-10-18 13:44:21 +0200

    feat: ListItem component and stories

---
+++ components/ListItem/Day.tsx
@@ -0,0 +1,39 @@
+import { format, fromUnixTime, getDate } from "date-fns";
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import { Text, useTheme } from "react-native-paper";
+
+type Props = {
+  timestamp: number;
+};
+
+const Day = ({ timestamp }: Props) => {
+  const theme = useTheme();
+
+  const date = fromUnixTime(timestamp);
+  const isToday =
+    format(date, "yyyy-MM-dd") === format(new Date(), "yyyy-MM-dd");
+
+  return (
+    <View style={styles.wrapper}>
+      <Text
+        variant="displaySmall"
+        style={[isToday && { color: theme.colors.tertiary }]}
+      >
+        {getDate(date)}
+      </Text>
+      <Text variant="bodyLarge">{format(date, "E")}</Text>
+    </View>
+  );
+};
+
+export default Day;
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    width: 35,
+  },
+});
