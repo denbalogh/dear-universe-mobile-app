8f53e76378078fb6183a209c7c0089f25d7ee00a
Denis Balogh <denbalogh@gmail.com>
2024-11-02 17:26:14 +0100

    feat: mood colors updated

--- components/MoodColor/values.ts
+++ components/MoodColor/values.ts
@@ -4,10 +4,9 @@ import { Mood } from "./types";
 export const moods = {
   "Happiness, Joy": { color: "#F4D03F", order: 1 },
   "Excitement, Energy": { color: "#E59866", order: 2 },
-  "Serenity, Balance": { color: "#58D68D", order: 3 },
-  "Calmness, Relaxation": { color: "#5DADE2", order: 4 },
-  "Anger, Frustration": { color: "#CB4335", order: 5 },
-  "Sadness, Indifference": { color: "#A6ACAF", order: 6 },
+  "Calmness, Relaxation": { color: "#48C9B0", order: 3 },
+  "Anger, Frustration": { color: "#CB4335", order: 4 },
+  "Sadness, Indifference": { color: "#A6ACAF", order: 5 },
 };
 
 export const getMoodColor = (mood: Mood): ColorValue => moods[mood]["color"];
