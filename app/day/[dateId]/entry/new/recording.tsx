a71882a54be8e14ca465dbc1ed47ddb0443eb1a7
Denis Balogh <denbalogh@gmail.com>
2024-12-13 11:29:15 +0100

    fix: when creating new entry for days without content create new day object

--- app/day/[dateId]/entry/new/recording.tsx
+++ app/day/[dateId]/entry/new/recording.tsx
@@ -4,7 +4,7 @@ import {
   useLocalSearchParams,
   useRouter,
 } from "expo-router";
-import React, { useCallback, useMemo, useState } from "react";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
 import { StyleSheet, View, BackHandler } from "react-native";
 import { Appbar, Text, useTheme } from "react-native-paper";
 import { spacing } from "@/constants/theme";
@@ -40,6 +40,16 @@ const NewEntryRecordingScreen = () => {
   const { dateId, comingFromScreen } =
     useLocalSearchParams<NewEntrySearchTermParams>();
   const dayObject = useObject(Day, dateId);
+
+  useEffect(() => {
+    if (dayObject === null) {
+      realm.write(() => {
+        realm.create(Day, {
+          _id: dateId,
+        });
+      });
+    }
+  }, [dateId, dayObject, realm]);
 
   const [isLoading, setIsLoading] = useState(false);
   const [recording, setRecording] = useState<Audio.Recording>();
@@ -202,7 +212,7 @@ const NewEntryRecordingScreen = () => {
   };
 
   const headline = useMemo(
-    () => `Creating recording for ${formatFullDate(parseDateId(dateId))}`,
+    () => `Creating entry for ${formatFullDate(parseDateId(dateId))}`,
     [dateId],
   );
 
