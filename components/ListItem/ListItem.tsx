c8cc27eea5b3e81c6f553be4c40d5ecfc6a9d1a0
Denis Balogh <denbalogh@gmail.com>
2025-04-16 15:35:50 +0200

    feat: infinite list item not elevated and ripple on press

--- components/ListItem/ListItem.tsx
+++ components/ListItem/ListItem.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { StyleSheet, View } from "react-native";
-import { Text, IconButton, Card, useTheme } from "react-native-paper";
+import {
+  Text,
+  IconButton,
+  useTheme,
+  TouchableRipple,
+} from "react-native-paper";
 import { roundness, sizing, spacing } from "@/constants/theme";
 import { format } from "date-fns/format";
 import { getDate } from "date-fns/getDate";
@@ -10,7 +15,6 @@ import { ITEM_HEIGHT } from "../InfiniteDaysList/constants";
 import { FEELING_GROUP_NAMES } from "@/constants/feelings";
 import FeelingsIndicator from "../FeelingsIndicator/FeelingsIndicator";
 import MediaPreview from "./MediaPreview";
-import CustomMenu from "../CustomMenu/CustomMenu";
 
 type Props = {
   title: string;
@@ -47,109 +51,90 @@ const ListItem = ({
     : theme.colors.onSurfaceVariant;
 
   return (
-    <CustomMenu
-      menuItems={[
-        {
-          title: "Add text",
-          leadingIcon: "pen-plus",
-          onPress: onAddTextPress,
-        },
-        {
-          title: "Add recording",
-          leadingIcon: "microphone-plus",
-          onPress: onAddRecordingPress,
-        },
-        {
-          title: "Add media",
-          leadingIcon: "image-plus",
-          onPress: onAddMediaPress,
-        },
-      ]}
+    <TouchableRipple
+      onPress={onPress}
+      style={styles.itemWrapper}
+      background={{
+        foreground: true,
+        borderless: false,
+      }}
     >
-      {({ openMenu }) => (
-        <Card
-          testID="ListItemPressable"
-          style={[styles.card, { backgroundColor }]}
-          onPress={onPress}
-          onLongPress={openMenu}
-        >
-          <Card.Content style={styles.cardContent}>
-            <View style={styles.dayWrapper}>
-              <Text
-                variant="displaySmall"
-                style={[
-                  { color: textColor },
-                  isToday && [styles.today, { color: theme.colors.tertiary }],
-                ]}
-                accessibilityLabel={
-                  isToday
-                    ? `Today ${format(date, "do LLLL yyyy")}`
-                    : format(date, "do LLLL yyyy")
-                }
-              >
-                {getDate(date)}
-              </Text>
-              <Text
-                variant="bodyLarge"
-                accessibilityLabel={format(date, "EEEE")}
-                style={{ color: textColor }}
-              >
-                {format(date, "E")}
-              </Text>
-            </View>
-            {isEmpty ? (
-              <View style={styles.addEntryButtonsWrapper}>
-                <IconButton
-                  icon="pen-plus"
-                  onPress={onAddTextPress}
-                  accessibilityLabel="Add text"
-                  size={sizing.sizeMedium}
-                />
-                <IconButton
-                  icon="microphone-plus"
-                  onPress={onAddRecordingPress}
-                  accessibilityLabel="Add recording"
-                  size={sizing.sizeMedium}
-                />
-                <IconButton
-                  icon="image-plus"
-                  onPress={onAddMediaPress}
-                  accessibilityLabel="Add media"
-                  size={sizing.sizeMedium}
-                />
-              </View>
-            ) : (
-              <View style={styles.contentWrapper}>
-                <Text
-                  style={[styles.title, { color: textColor }]}
-                  variant="bodyMedium"
-                  numberOfLines={4}
-                >
-                  {title || "No title for the day"}
-                </Text>
-                <MediaPreview dateId={dateId} />
-              </View>
-            )}
-            <FeelingsIndicator
-              style={styles.feelingsIndicator}
-              feelings={feelings}
+      <View style={[styles.itemContentWrapper, { backgroundColor }]}>
+        <View style={styles.dayWrapper}>
+          <Text
+            variant="displaySmall"
+            style={[
+              { color: textColor },
+              isToday && [styles.today, { color: theme.colors.tertiary }],
+            ]}
+            accessibilityLabel={
+              isToday
+                ? `Today ${format(date, "do LLLL yyyy")}`
+                : format(date, "do LLLL yyyy")
+            }
+          >
+            {getDate(date)}
+          </Text>
+          <Text
+            variant="bodyLarge"
+            accessibilityLabel={format(date, "EEEE")}
+            style={{ color: textColor }}
+          >
+            {format(date, "E")}
+          </Text>
+        </View>
+        {isEmpty ? (
+          <View style={styles.addEntryButtonsWrapper}>
+            <IconButton
+              icon="pen-plus"
+              onPress={onAddTextPress}
+              accessibilityLabel="Add text"
+              size={sizing.sizeMedium}
             />
-          </Card.Content>
-        </Card>
-      )}
-    </CustomMenu>
+            <IconButton
+              icon="microphone-plus"
+              onPress={onAddRecordingPress}
+              accessibilityLabel="Add recording"
+              size={sizing.sizeMedium}
+            />
+            <IconButton
+              icon="image-plus"
+              onPress={onAddMediaPress}
+              accessibilityLabel="Add media"
+              size={sizing.sizeMedium}
+            />
+          </View>
+        ) : (
+          <View style={styles.contentWrapper}>
+            <Text
+              style={[styles.title, { color: textColor }]}
+              variant="bodyMedium"
+              numberOfLines={4}
+            >
+              {title || "No title for the day"}
+            </Text>
+            <MediaPreview dateId={dateId} />
+          </View>
+        )}
+        <FeelingsIndicator
+          style={styles.feelingsIndicator}
+          feelings={feelings}
+        />
+      </View>
+    </TouchableRipple>
   );
 };
 
 export default ListItem;
 
 const styles = StyleSheet.create({
-  card: {
+  itemWrapper: {
     width: "100%",
     height: ITEM_HEIGHT,
     borderRadius: roundness,
+    overflow: "hidden",
   },
-  cardContent: {
+  itemContentWrapper: {
     flexDirection: "row",
     alignItems: "stretch",
     borderRadius: roundness,
